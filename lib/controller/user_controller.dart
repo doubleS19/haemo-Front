d6483a889fe13cb9ba2438720b85ffbb186e91ae
mi-fasol <minyoong39@naver.com>
2023-03-25 20:31:19 +0900

    fix: 유저 컨트롤러 로직 수정

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -1,34 +1,33 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hae_mo/page/home_page.dart';
+import "dart:developer" as dev;
 
 enum RegisterState { Success, Fail, Empty, Okay }
 
 class UserController extends GetxController {
   RegisterState _registerState = RegisterState.Fail;
 
-  Future<bool> CheckUserInfo(
-      String nickname, String major, String gender) async {
-    if (nickname != "" && major != "학과 선택") {
+  RegisterState get registerState => _registerState;
+
+  Future checkUserInfo(String nickname, String major, String gender) async {
+    if (nickname.isNotEmpty && major != "학과 선택" && gender != "성별") {
       _registerState = RegisterState.Okay;
-      return true;
+      dev.log("Okay~");
     } else {
       _registerState = RegisterState.Empty;
-      return false;
+      dev.log("Empty~");
     }
     update();
   }
 
-  Future<bool> SaveInfo(String nickname, String major, String gender) async {
+  Future saveInfo(String nickname, String major, String gender) async {
     if (_registerState == RegisterState.Okay) {
       _registerState = RegisterState.Success;
-      Get.to(HomePage());
-      return true;
+      Get.to(const HomePage());
+      dev.log("Success~");
     } else {
       _registerState = RegisterState.Fail;
-      return false;
+      dev.log("Fail~");
     }
     update();
   }
--- lib/page/register_page.dart
+++ lib/page/register_page.dart
@@ -33,16 +33,19 @@ class _RegisterPageState extends State<RegisterPage> {
   final _majorList = ["학과 선택", "컴퓨터공학과", "소프트웨어공학과", "인공지능학과"];
   var _selectedMajor = "학과 선택";
 
-  final _genderList = ["남자", "여자"];
-  var _selectedGender = "남자";
+  final _genderList = ["성별", "남자", "여자"];
+  var _selectedGender = "성별";
 
   final TextEditingController _textController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   final UserController _userController = Get.put(UserController());
-  final RegisterState _registerState = RegisterState.Fail;
+
+  late RegisterState _registerState;
 
   @override
   Widget build(BuildContext context) {
+    _registerState = _userController.registerState;
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
@@ -52,90 +55,110 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
         body: Container(
           margin: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _textController,
-                decoration: const InputDecoration(
-                  labelText: 'Nickname',
-                  hintText: '닉네임을 입력하세요.',
-                  labelStyle: TextStyle(color: Colors.blueGrey),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide(width: 1, color: Colors.blueGrey),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide(width: 1, color: Colors.blueGrey),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              Container(
-                  alignment: Alignment.centerLeft,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        DropdownButton(
-                          value: _selectedMajor,
-                          items: _majorList.map((value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            _userController.CheckUserInfo(_textController.text,
-                                _selectedMajor, _selectedGender);
-                            setState(() {
-                              _selectedMajor = value!;
-                            });
+          child: GetBuilder<UserController>(
+            builder: (_userController) {
+              return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Form(
+                        key: _formKey,
+                        child: TextFormField(
+                          controller: _textController,
+                          decoration: const InputDecoration(
+                            labelText: 'Nickname',
+                            hintText: '닉네임을 입력하세요.',
+                            labelStyle: TextStyle(color: Colors.blueGrey),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0)),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.blueGrey),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0)),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.blueGrey),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0)),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              _registerState = RegisterState.Empty;
+                            }
                           },
-                        ),
-                        DropdownButton(
-                          value: _selectedGender,
-                          items: _genderList.map((value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            setState(() {
-                              _selectedGender = value!;
-                            });
-                            _userController.CheckUserInfo(_textController.text,
-                                _selectedMajor, _selectedGender);
-                          },
-                        ),
-                      ])),
-              const SizedBox(height: 60),
-              if (_registerState == RegisterState.Empty) ...[
-                SizedBox(
-                    height: 45.0,
-                    width: MediaQuery.of(context).size.width,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blueGrey),
-                      onPressed: null,
-                      child: const Text("가입"),
-                    ))
-              ] else ...[
-                SizedBox(
-                    height: 45.0,
-                    width: MediaQuery.of(context).size.width,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Get.to(const HomePage());
-                      },
-                      child: const Text("가입"),
-                    ))
-              ]
-            ],
+                        )),
+                    const SizedBox(height: 30),
+                    Container(
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              DropdownButton(
+                                value: _selectedMajor,
+                                items: _majorList.map((value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  _userController.checkUserInfo(
+                                      _textController.text,
+                                      _selectedMajor,
+                                      _selectedGender);
+                                  setState(() {
+                                    _selectedMajor = value!;
+                                  });
+                                },
+                              ),
+                              DropdownButton(
+                                value: _selectedGender,
+                                items: _genderList.map((value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _selectedGender = value!;
+                                  });
+                                  _userController.checkUserInfo(
+                                      _textController.text,
+                                      _selectedMajor,
+                                      _selectedGender);
+                                },
+                              ),
+                            ])),
+                    const SizedBox(height: 60),
+                    if (_registerState == RegisterState.Fail ||
+                        _registerState == RegisterState.Empty) ...[
+                      SizedBox(
+                          height: 45.0,
+                          width: MediaQuery.of(context).size.width,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blueGrey),
+                            onPressed: null,
+                            child: const Text("가입"),
+                          ))
+                    ] else ...[
+                      SizedBox(
+                          height: 45.0,
+                          width: MediaQuery.of(context).size.width,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Get.to(const HomePage());
+                            },
+                            child: const Text("가입"),
+                          ))
+                    ]
+                  ]);
+            },
           ),
         ));
   }
