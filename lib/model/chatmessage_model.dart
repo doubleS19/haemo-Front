cac9e9aaeb06fd55d72ce289d69f160d8cbf0bac
seoyeon <seoyeonn051@gmail.com>
2023-04-03 17:29:46 +0900

    fix: chatMessage 모델 수정

--- lib/model/chatmessage_model.dart
+++ lib/model/chatmessage_model.dart
@@ -1,17 +1,49 @@
 import 'dart:ffi';
-import 'dart:ui';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatMessage{
+
+class ChatUser {
+  String? name;
+  Int? profileImage;
+}
+
+class ChatMessage {
   String? text;
-  String? image;
   String? sender;
-  String? createdAt;
+  Timestamp? createdAt;
+}
 
-  ChatMessage({
-    required this.text,
-    required this.image,
-    required this.sender,
-    required this.createdAt,
+
+class ChatData {
+  ChatUser? chatUser1;
+  ChatUser? chatUser2;
+  List<ChatMessage>? chatMessageList;
+
+  ChatData({
+    required this.chatUser1,
+    required this.chatUser2,
+    required this.chatMessageList
   });
+
+  // Map to data
+  factory ChatData.fromMap({
+    required ChatUser chatUser1,
+    required ChatUser chatUser2,
+    required List<ChatMessage> chatMessageList }){
+    return ChatData(
+      chatUser1: chatUser1,
+      chatUser2: chatUser2,
+      chatMessageList: chatMessageList
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    Map<String, dynamic> data = {};
+    data['chatUser1'] = chatUser1;
+    data['chatUser2'] = chatUser2;
+    data['chatMessageList'] = chatMessageList;
+    return data;
+  }
+
 
 }
