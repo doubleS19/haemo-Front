2aedbe3f1664d6168de06a30a3c480209c107b15
mi-fasol <minyoong39@naver.com>
2023-04-13 00:46:44 +0900

    design: 회원 가입 페이지 디자인

--- lib/page/register_page.dart
+++ lib/page/register_page.dart
@@ -47,71 +47,129 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     _registerState = _userController.registerState;
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          foregroundColor: Colors.black,
-          elevation: 0.0,
-          automaticallyImplyLeading: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0.0,
+        title: const Text(
+          "회원가입",
+          style: TextStyle(
+            color: Color(0xff595959),
+            fontSize: 19.0,
+          ),
         ),
-        body: Container(
-          margin: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
-          child: GetBuilder<UserController>(
-            builder: (_userController) {
-              return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Form(
-                        key: _formKey,
-                        child: TextFormField(
-                          controller: _textController,
-                          decoration: const InputDecoration(
-                            labelText: 'Nickname',
-                            hintText: '닉네임을 입력하세요.',
-                            labelStyle: TextStyle(color: Colors.blueGrey),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10.0)),
-                              borderSide:
-                                  BorderSide(width: 1, color: Colors.blueGrey),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10.0)),
-                              borderSide:
-                                  BorderSide(width: 1, color: Colors.blueGrey),
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10.0)),
+        automaticallyImplyLeading: true,
+      ),
+      body: GetBuilder<UserController>(
+        builder: (_userController) {
+          return Column(children: [
+            const Divider(color: Color(0xff3ac7e7)),
+            Container(
+                margin: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+                alignment: Alignment.topCenter,
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Column(children: [
+                  const Text(
+                    "헤쳐모여 TUK",
+                    style:
+                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.w200),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  const Text(
+                    "프로필",
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Color(0xff818181),
+                        fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        color: const Color(0xffe3e3e3),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    alignment: Alignment.center,
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0),
+                    alignment: Alignment.centerLeft,
+                    child: const Text(
+                      "닉네임",
+                      style: TextStyle(
+                          color: Color(0xff393939),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        controller: _textController,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xffe3e3e3),
+                          hintText: '닉네임을 입력하세요.',
+                          hintStyle: TextStyle(
+                              color: Color(0xff818181), fontSize: 15.0),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0)),
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xffe3e3e3)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0)),
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xffe3e3e3)),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0)),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            _registerState = RegisterState.empty;
+                          }
+                        },
+                      )),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "성별",
+                              style: TextStyle(
+                                  color: Color(0xff393939),
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w800),
                             ),
                           ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              _registerState = RegisterState.empty;
-                            }
-                          },
-                        )),
-                    const SizedBox(height: 30),
-                    Container(
-                        alignment: Alignment.centerLeft,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              DropdownButton(
-                                value: _selectedMajor,
-                                items: _majorList.map((value) {
-                                  return DropdownMenuItem(
-                                    value: value,
-                                    child: Text(value),
-                                  );
-                                }).toList(),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedMajor = value!;
-                                  });
-                                },
-                              ),
-                              DropdownButton(
+                          const SizedBox(
+                            height: 5.0,
+                          ),
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: DropdownButton(
                                 value: _selectedGender,
                                 items: _genderList.map((value) {
                                   return DropdownMenuItem(
@@ -128,35 +186,87 @@ class _RegisterPageState extends State<RegisterPage> {
                                       _selectedMajor,
                                       _selectedGender);
                                 },
-                              ),
-                            ])),
-                    const SizedBox(height: 60),
-                    if (_registerState == RegisterState.fail ||
-                        _registerState == RegisterState.empty) ...[
-                      SizedBox(
-                          height: 45.0,
-                          width: MediaQuery.of(context).size.width,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.blueGrey),
-                            onPressed: null,
-                            child: const Text("가입"),
-                          ))
-                    ] else ...[
-                      SizedBox(
-                          height: 45.0,
-                          width: MediaQuery.of(context).size.width,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              _userController.saveInfo(_textController.text,
-                                  _selectedMajor, _selectedGender);
-                            },
-                            child: const Text("가입"),
-                          ))
-                    ]
-                  ]);
-            },
-          ),
-        ));
+                              )),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "전공",
+                              style: TextStyle(
+                                  color: Color(0xff393939),
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5.0,
+                          ),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: DropdownButton(
+                              value: _selectedMajor,
+                              items: _majorList.map((value) {
+                                return DropdownMenuItem(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (value) {
+                                setState(() {
+                                  _selectedMajor = value!;
+                                });
+                              },
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ])),
+            const SizedBox(height: 30),
+            if (_registerState == RegisterState.fail ||
+                _registerState == RegisterState.empty) ...[
+              Container(
+                  margin: const EdgeInsets.only(right: 20, left: 20.0),
+                  height: 40.0,
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueGrey),
+                    onPressed: null,
+                    child: const Text(
+                      "등록",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.w300),
+                    ),
+                  ))
+            ] else ...[
+              Container(
+                  margin: const EdgeInsets.only(right: 20, left: 20.0),
+                  height: 40.0,
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff3ac7e7),
+                    ),
+                    onPressed: () {
+                      _userController.saveInfo(_textController.text,
+                          _selectedMajor, _selectedGender);
+                    },
+                    child: const Text(
+                      "등록",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.w300),
+                    ),
+                  ))
+            ]
+          ]);
+        },
+      ),
+    );
   }
 }
