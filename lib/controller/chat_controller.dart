75478e712775d02c7923f76b097d037c0a3f8db3
seoyeon <seoyeonn051@gmail.com>
2023-04-13 18:15:00 +0900

    fix: 채팅창 갈아엎기

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -1,20 +1,137 @@
+import 'dart:async';
+import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:hae_mo/page/chat_list_page.dart';
+import 'package:http/http.dart';
+
+import '../model/chatmessage_model.dart';
+import '../model/shared_preference.dart';
 
 class ChatController extends GetxController {
   final firestore = FirebaseFirestore.instance;
+  late String chatRoomId = "";
+  final RxString _text = "".obs;
+  final RxString _sender = "".obs;
+  final createdAt = DateTime.now();
+  late List<ChatMessage> chatMessageList = [];
 
-  getData(){
-    var result = firestore.collection('haemo').doc('Koq0ZXc2Scnj1n6VcOyi').snapshots();
+  get text => _text;
+  get sender => _sender;
 
-    return result;
+  StreamController<List<ChatMessage>> streamController = StreamController<List<ChatMessage>>();
+
+  var message = ChatMessage(
+      text: "",
+      sender: "",
+      createdAt: DateTime.now(),
+      isRead: false
+  ).obs;
+
+  change({
+    required String text,
+    required String sender,
+  }) {
+    message.update((message) {
+      message?.text = text;
+      message?.sender = sender;
+      message?.createdAt = DateTime.now();
+      message?.isRead = false;
+    });
+  }
+
+  void startStream(){
+
   }
 
   @override
-  void onInit(){
+  void onInit() {
     super.onInit();
-    getData();
+
+
+    ever(message, (_) {
+      chatMessageList.add(message.value);
+      for (var i in chatMessageList) {
+        print(i.text);
+      }
+    });
   }
 
+  // 최초 메세지 전송 시 채팅방 생성 필요
+  void createChatroom(ChatUser otherUser, ChatMessage chatMessage) {
+    // me, other 사이에 생성된 채팅방이 있는지 확인 - 게시물에서 채팅으로 넘어갈 때 필요할 듯?
+    // chatRoomId 여기서 생성해야 될 듯?
+
+    late String createChatRoomId = "";
+    var chatroom = ChatData(
+      chatRoomId: createChatRoomId,
+      chatUser1: ChatUser(
+/*          studentId: PreferenceUtil.getString("studentId")!,
+          profileImage: PreferenceUtil.getInt("profileImage") != null? PreferenceUtil.getInt("profileImage")! : 1*/
+        studentId: "seoyeon",
+        profileImage: 1
+      ),
+      chatUser2: otherUser,
+      chatMessageList: [chatMessage]
+    );
+
+    streamController.sink.add([chatMessage]);
+
+    firestore.collection('haemo').doc("first")
+        .set(chatroom.toJson());
+
+    // 자신의 채팅방 리스트에 추가해야 됨
+    // UI 변화도 - Stream에서 메세지 받게 해서 할까?
+  }
+
+
+  // 메세지 전송 클릭 시 FireStore로 전송
+  void sendData(String chatRoomId, ChatUser chatUser, ChatMessage chatMessage) async{
+    try {
+      final snapshot = await firestore.collection('haemo').doc(chatRoomId).get();
+
+      if(snapshot.exists){
+
+
+      } else{
+        createChatroom(chatUser ,chatMessage);
+      }
+      change(text: text,
+          sender: PreferenceUtil.getString("studentId")!,
+      );
+
+/*      firestore.collection('haemo/$chatRoomId/chat/messageList/message').update(
+          message.value.toMap()).then((value) =>
+          print("DocumentSnapshot added with ID: ${value.id}")
+      );*/
+    } catch (ex) {
+      log('error', error: ex.toString(), stackTrace: StackTrace.current);
+    }
+  }
+
+  // 메세지 읽었을 때 false -> true
+
+  // 메시지 가져오기 - stream
+  Stream<ChatData> streamChatMessage(String chatRoomId) {
+    try {
+      // charRoomId가 없으면 조회되지 않음
+
+      final snapshots = firestore
+          .collection('haemo')
+          .doc(chatRoomId)
+          .snapshots()
+          .map((event) => ChatData.fromDocumentSnapshot(event));
+
+      snapshots.listen((event){
+        streamController.sink.add(event.chatMessageList!!);
+      });
+      return snapshots;
+    } catch (ex) {
+      log('error: ', error: ex.toString(), stackTrace: StackTrace.current);
+      return Stream.error(ex.toString());
+    }
+  }
 }
--- lib/page/chat_room_page.dart
+++ lib/page/chat_room_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:hae_mo/controller/chat_controller.dart';
 import 'package:hae_mo/utils/chage_time_format.dart';
 import '../model/chatlist_model.dart';
 import '../model/chatmessage_model.dart';
+import '../model/shared_preference.dart';
 
 class ChatRoom extends StatefulWidget {
   const ChatRoom({Key? key}) : super(key: key);
@@ -13,13 +18,8 @@ class ChatRoom extends StatefulWidget {
 }
 
 class _ChatRoomState extends State<ChatRoom> {
-  final List<ChatMessage> chatList = [
-    ChatMessage(
-        text: "text", sender: "sender", createdAt: DateTime.now()),
-    ChatMessage(
-        text: "text", sender: "receiver", createdAt: DateTime.now()),
-  ];
   final TextEditingController _textController = TextEditingController();
+  final controller = Get.put(ChatController());
 
   Widget receiver(String text, String name, DateTime time, dynamic profile) {
     return Column(
@@ -49,7 +49,8 @@ class _ChatRoomState extends State<ChatRoom> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(height: 22),
-                Text(changeDatetimeToString(time), style: const TextStyle(fontSize: 12, color: Colors.black26))
+                Text(changeDatetimeToString(time),
+                    style: const TextStyle(fontSize: 12, color: Colors.black26))
               ],
             )),
           ],
@@ -66,7 +67,7 @@ class _ChatRoomState extends State<ChatRoom> {
             children: [
               SizedBox(height: 18),
               Text(changeDatetimeToString(time),
-                  style: TextStyle(fontSize: 12,  color: Colors.black26)),
+                  style: TextStyle(fontSize: 12, color: Colors.black26)),
             ],
           ),
           SizedBox(width: 5),
@@ -98,21 +99,17 @@ class _ChatRoomState extends State<ChatRoom> {
     );
   }
 
-  void _handleSubmitted(text) {
+  _handleSubmitted() {
+    var text = _textController.text;
     _textController.clear();
-
-    setState(() {
-      chatList.add(
-          ChatMessage(
-              text: text, sender: "sender", createdAt: DateTime.now()
-/*          DateFormat("a K:a")
-              .format(DateTime.now())
-              .replaceAll("AM", "오전")
-              .replaceAll("PM", "오후"))*/
-          ));
-    });
+    controller.streamController.sink.add([
+      ChatMessage(
+          text: text,
+          sender: PreferenceUtil.getString("studentId")!,
+          createdAt: DateTime.now(),
+          isRead: false)
+    ]);
   }
-
 
   Widget chooseSender(ChatMessage chat) {
     // sharedpreference에 저장된 아이디라면
@@ -140,10 +137,7 @@ class _ChatRoomState extends State<ChatRoom> {
             ),
             actions: [
               IconButton(
-                icon: const Icon(
-                  Icons.report_gmailerrorred_rounded,
-                  size: 25
-                ),
+                icon: const Icon(Icons.report_gmailerrorred_rounded, size: 25),
                 onPressed: () {},
               ),
               const SizedBox(width: 8)
@@ -156,7 +150,36 @@ class _ChatRoomState extends State<ChatRoom> {
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Column(
                 children: [
-                  for (var chat in chatList) chooseSender(chat)
+                  StreamBuilder(
+                    stream: controller.streamController.stream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<dynamic> snapshot) {
+                      if (snapshot.hasData) {
+                        List<ChatMessage> listMessage = snapshot.data;
+
+                        return ListView.builder(
+                          itemCount: listMessage.length,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (BuildContext context, int index) {
+                            if (listMessage[index].sender == "sender") {
+                              return sender(listMessage[index].text!,
+                                  listMessage[index].createdAt!);
+                            } else {
+                              return receiver(
+                                  listMessage[index].text!,
+                                  listMessage[index].sender!,
+                                  listMessage[index].createdAt!,
+                                  1);
+                            }
+                          },
+                        );
+                      } else {
+                        return const Center(child: Text("a"));
+                      }
+                    },
+                  ),
+                  for (var chat in controller.chatMessageList)
+                    chooseSender(chat)
 
 /*                    ListView.builder(
                       itemCount: chats.length,
@@ -184,15 +207,15 @@ class _ChatRoomState extends State<ChatRoom> {
                             child: TextField(
                       controller: _textController,
                       maxLines: 1,
-                      style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
+                      style: const TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
                         focusedBorder: InputBorder.none,
                         enabledBorder: InputBorder.none,
                       ),
-                      onSubmitted: _handleSubmitted,
+                      onSubmitted: _handleSubmitted(),
                     ))),
-                    chatIconButton(Icon(FontAwesomeIcons.faceSmile)),
-                    chatIconButton(Icon(FontAwesomeIcons.gear))
+                    chatIconButton(const Icon(FontAwesomeIcons.faceSmile)),
+                    chatIconButton(const Icon(FontAwesomeIcons.gear))
                   ],
                 ))
           ])),
