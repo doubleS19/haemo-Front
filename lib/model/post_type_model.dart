54afb27affdf05adf435d01e41a76ad4b0a30d5a
seoyeon <seoyeonn051@gmail.com>
2023-07-03 02:02:49 +0900

    fix: customButton 삭제 및 post type model 수정

--- lib/model/post_type_model.dart
+++ lib/model/post_type_model.dart
@@ -6,20 +6,20 @@ enum PostType{
 
 final postingTextField = [
   "제목을 입력해주세요",
-  "모임에 대한 내용을 입력해주세요",
+  //"모임에 대한 내용을 입력해주세요",
 ];
 
 final clubPostingTextField = [
   "제목을 입력해주세요",
   "소모임 소개내용을 입력해주세요",
-  "#해시태그를 입력해주세요",
-  "소모임에 대한 내용을 입력해주세요"
+  //"#해시태그를 입력해주세요",
+  //"소모임에 대한 내용을 입력해주세요"
 ];
 
 final hotPlacePostingTextField = [
   "장소를 입력해주세요",
   "핫플에 대한 간단한 설명을 입력해주세요",
-  "핫플에 대해 내용을 입력해주세요",
+  //"핫플에 대해 내용을 입력해주세요",
 ];
 
 class Post {
@@ -41,20 +41,20 @@ class Post {
     List<String> description = [];
 
     switch (type) {
-      case PostType.meeting:
+      case PostType.hotPlace:
         appBarText = "핫플 등록";
         title = ["장소", "설명"];
+        description = hotPlacePostingTextField;
+        break;
+      case PostType.meeting:
+        appBarText = "모임 등록";
+        title = ["모임"];
         description = postingTextField;
         break;
       case PostType.club:
-        appBarText = "모임 등록";
-        title = ["모임"];
-        description = clubPostingTextField;
-        break;
-      case PostType.hotPlace:
         appBarText = "소모임 등록";
         title = ["소모임", "설명"];
-        description = hotPlacePostingTextField;
+        description = clubPostingTextField;
         break;
     }
 
