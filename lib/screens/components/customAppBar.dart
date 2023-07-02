0ff65528d4b09710c662eae865caadba6c24ad84
seoyeon <seoyeonn051@gmail.com>
2023-07-03 02:02:05 +0900

    fix: custom Appbar & Textfield 수정

--- lib/screens/components/customAppBar.dart
+++ lib/screens/components/customAppBar.dart
@@ -1,15 +1,19 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 Widget customAppbar(String appBarText) {
   return Builder(
       builder: (context) => AppBar(
-          title: Text(appBarText, style: Theme.of(context).textTheme.headlineMedium),
-          centerTitle: true,
-          backgroundColor: Theme.of(context).primaryColor,
-          actions: const <Widget>[
-/*            IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.navigate_before))*/
-          ]));
+            title: Text(appBarText,
+                style: Theme.of(context).textTheme.headlineMedium),
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () {
+              },
+              color: Theme.of(context).iconTheme.color,
+              icon: const Icon(Icons.arrow_back),
+            ),
+            backgroundColor: Theme.of(context).primaryColor,
+          ));
 }
--- lib/screens/components/customTextField.dart
+++ lib/screens/components/customTextField.dart
@@ -1,56 +1,33 @@
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-final postingTextField = [
-  "제목을 입력해주세요",
-  "모임에 대한 내용을 입력해주세요",
-];
 
-final clubPostingTextField = [
-  "제목을 입력해주세요",
-  "소모임 소개내용을 입력해주세요",
-  "#해시태그를 입력해주세요",
-  "소모임에 대한 내용을 입력해주세요"
-];
 
-final hotPlacePostingTextField = [
-  "장소를 입력해주세요",
-  "핫플에 대한 간단한 설명을 입력해주세요",
-  "핫플에 대해 내용을 입력해주세요",
-];
-
-Widget registerUserInfoTextField(TextEditingController _textEdictionController){
-  return SafeArea(child: Center(
-    child: TextFormField(
-
-    )
-  ));
+Widget registerUserInfoTextField(
+    TextEditingController _textEdictionController) {
+  return SafeArea(child: Center(child: TextFormField()));
 }
 
-Widget postingPageTitleTextField(void Function()? onClick, String hintText, TextEditingController textEdController) {
-  final TextEditingController _textController = TextEditingController();
-
-  return SafeArea(child: Center(
-      child: TextFormField(
-        enabled: false,
-        decoration: InputDecoration(
-            hintText: hintText
-        ),
-        controller: textEdController,
-      )
-  ));
+Widget postingPageTitleTextField(
+    String hintText, TextEditingController textEdController, dynamic context) {
+  return TextFormField(
+    enabled: true,
+    decoration: InputDecoration(hintText: hintText, hintStyle: Theme.of(context).textTheme.bodySmall, isDense: true ),
+    controller: textEdController,
+  );
 }
 
-Widget postingPageDetailTextField(void Function()? onClick, String hintText, TextEditingController textEdController) {
+Widget postingPageDetailTextField(void Function()? onClick, String hintText,
+    TextEditingController textEdController) {
   final TextEditingController _textController = TextEditingController();
 
-  return SafeArea(child: Center(
-      child: TextFormField(
-        enabled: false,
-        decoration: InputDecoration(
-            hintText: hintText
-        ),
-        controller: textEdController,
-      )
-  ));
+  return SafeArea(
+      child: Center(
+          child: TextFormField(
+    enabled: false,
+    decoration: InputDecoration(hintText: hintText),
+    controller: textEdController,
+  )));
 }
