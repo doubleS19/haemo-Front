420addc6871eecdc343bc6bee8522f49d1e6a0ea
mi-fasol <minyoong39@naver.com>
2023-07-08 23:47:56 +0900

    feat: 24시간 남은 게시물 가져오는 기능 추가

--- lib/service/db_service.dart
+++ lib/service/db_service.dart
@@ -14,7 +14,8 @@ class DBService {
   Future<void> saveUser(User user) async {
     try {
       final response = await http.post(
-        Uri.parse("http://43.201.211.1:1004/user"),
+        // Uri.parse("http://43.201.211.1:1004/user"),
+        Uri.parse("http://localhost:1004/user"),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
@@ -34,7 +35,8 @@ class DBService {
   Future<void> savePost(Post post) async {
     try {
       final response = await http.post(
-        Uri.parse("http://43.201.211.1:1004/post"),
+        // Uri.parse("http://43.201.211.1:1004/post"),
+        Uri.parse("http://localhost:1004/post"),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
@@ -52,7 +54,22 @@ class DBService {
   }
 
   Future<List<PostResponse>> getAllPost() async {
-    final response = await http.get(Uri.parse("http://43.201.211.1:1004/post"));
+    // final response = await http.get(Uri.parse("http://43.201.211.1:1004/post"));
+    final response = await http.get(Uri.parse("http://localhost:1004/post"));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List<dynamic>;
+      return data
+          .map<PostResponse>((json) => PostResponse.fromJson(json))
+          .toList();
+    } else {
+      throw Exception('Failed to load post llist');
+    }
+  }
+
+  Future<List<PostResponse>> get24HoursPosts() async {
+    // final response = await http.get(Uri.parse("http://43.201.211.1:1004/post/24hours"));
+    final response =
+        await http.get(Uri.parse("http://localhost:1004/post/24hours"));
     if (response.statusCode == 200) {
       final data = json.decode(response.body) as List<dynamic>;
       return data
@@ -65,7 +82,8 @@ class DBService {
 
   Future<Post> getPostById(int id) async {
     final response =
-        await http.get(Uri.parse("http://43.201.211.1:1004/post/$id"));
+        // await http.get(Uri.parse("http://43.201.211.1:1004/post/$id"));
+        await http.get(Uri.parse("http://localhost:1004/post/$id"));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body) as Map<String, dynamic>;
       return Post.fromJson(jsonData);
@@ -78,7 +96,8 @@ class DBService {
 
   Future<UserResponse> getUserByPost(int pId) async {
     final response = await http
-        .get(Uri.parse("http://43.201.211.1:1004/post/postUser/$pId"));
+        // .get(Uri.parse("http://43.201.211.1:1004/post/postUser/$pId"));
+        .get(Uri.parse("http://localhost:1004/post/postUser/$pId"));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body) as Map<String, dynamic>;
       return UserResponse.fromJson(jsonData);
