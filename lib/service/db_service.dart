05508dfeb422acbf598dd5ac19d5d5fb3853cfc9
mi-fasol <minyoong39@naver.com>
2023-07-25 22:44:09 +0900

    fix: db 서비스 파일 오류 메시지 변경

--- lib/service/db_service.dart
+++ lib/service/db_service.dart
@@ -74,7 +74,7 @@ class DBService {
   Future<List<PostResponse>> get24HoursPosts() async {
     // final response = await http.get(Uri.parse("http://43.201.211.1:1004/post/24hours"));
     final response =
-    await http.get(Uri.parse("http://localhost:1004/post/24hours"));
+        await http.get(Uri.parse("http://localhost:1004/post/24hours"));
     if (response.statusCode == 200) {
       final data = json.decode(response.body) as List<dynamic>;
       return data
@@ -87,8 +87,8 @@ class DBService {
 
   Future<Post> getPostById(int id) async {
     final response =
-    // await http.get(Uri.parse("http://43.201.211.1:1004/post/$id"));
-    await http.get(Uri.parse("http://localhost:1004/post/$id"));
+        // await http.get(Uri.parse("http://43.201.211.1:1004/post/$id"));
+        await http.get(Uri.parse("http://localhost:1004/post/$id"));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body) as Map<String, dynamic>;
       return Post.fromJson(jsonData);
@@ -101,7 +101,7 @@ class DBService {
 
   Future<UserResponse> getUserByPost(int pId) async {
     final response = await http
-    // .get(Uri.parse("http://43.201.211.1:1004/post/postUser/$pId"));
+        // .get(Uri.parse("http://43.201.211.1:1004/post/postUser/$pId"));
         .get(Uri.parse("http://localhost:1004/post/postUser/$pId"));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body) as Map<String, dynamic>;
@@ -117,7 +117,7 @@ class DBService {
     // final response =
     //     await http.get(Uri.parse("http://43.201.211.1:1004/user/$nickname"));
     final response =
-    await http.get(Uri.parse("http://localhost:1004/user/$nickname"));
+        await http.get(Uri.parse("http://localhost:1004/user/$nickname"));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body) as Map<String, dynamic>;
       return UserResponse.fromJson(jsonData);
@@ -127,7 +127,6 @@ class DBService {
       throw Exception("Failed to fetch User by Nickname");
     }
   }
-
 
   Future<bool> saveClubPost(ClubPost post) async {
     try {
@@ -167,13 +166,13 @@ class DBService {
 
   Future<ClubPost> getClubPostById(int id) async {
     final response =
-    // await http.get(Uri.parse("http://43.201.211.1:1004/club/$id"));
-    await http.get(Uri.parse("http://localhost:1004/club/$id"));
+        // await http.get(Uri.parse("http://43.201.211.1:1004/club/$id"));
+        await http.get(Uri.parse("http://localhost:1004/club/$id"));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body) as Map<String, dynamic>;
       return ClubPost.fromJson(jsonData);
     } else if (response.statusCode == 404) {
-      throw Exception("Post not found");
+      throw Exception("Club Post not found");
     } else {
       throw Exception("Failed to fetch Post by Id");
     }
@@ -181,7 +180,7 @@ class DBService {
 
   Future<UserResponse> getUserByClubPost(int pId) async {
     final response = await http
-    // .get(Uri.parse("http://43.201.211.1:1004/club/clubPostUser/$pId"));
+        // .get(Uri.parse("http://43.201.211.1:1004/club/clubPostUser/$pId"));
         .get(Uri.parse("http://localhost:1004/club/clubPostUser/$pId"));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body) as Map<String, dynamic>;
@@ -209,7 +208,9 @@ class DBService {
     try {
       final response = await http.post(
         // Uri.parse("http://43.201.211.1:1004/club"),
-        Uri.parse("http://localhost:1004/hotplace"),  /// 링크 수정 필요(임의로 설정)
+        Uri.parse("http://localhost:1004/hotplace"),
+
+        /// 링크 수정 필요(임의로 설정)
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
