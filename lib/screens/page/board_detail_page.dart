1079838134b8bd640ce696ddb76e457d2a40ef04
mi-fasol <minyoong39@naver.com>
2023-08-04 19:10:27 +0900

    fix: club detail page -> BoardDetailPage로 가능하게 변경

--- lib/screens/page/board_detail_page.dart
+++ lib/screens/page/board_detail_page.dart
@@ -5,6 +5,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:hae_mo/common/color.dart';
 import 'package:hae_mo/controller/meeting_page_controller.dart';
+import 'package:hae_mo/model/club_post_model.dart';
 import 'package:hae_mo/model/user_response_model.dart';
 import '../../model/comment_response_model.dart';
 import '../../model/post_model.dart';
@@ -41,12 +42,12 @@ class _BoardDetailPageState extends State<BoardDetailPage> {
           automaticallyImplyLeading: true,
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Divider(
-                color: AppTheme.mainColor,
-                thickness: 1.0,
-              ),
+          child: Column(children: [
+            Divider(
+              color: AppTheme.mainColor,
+              thickness: 1.0,
+            ),
+            if (widget.type == 1) ...[
               FutureBuilder(
                 future: db.getPostById(widget.pId),
                 builder: (context, snapshot) {
@@ -173,8 +174,135 @@ class _BoardDetailPageState extends State<BoardDetailPage> {
                   );
                 },
               ),
+            ] else ...[
+              FutureBuilder(
+                future: db.getClubPostById(widget.pId),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final ClubPost post = snapshot.data as ClubPost;
+                    return FutureBuilder(
+                      future: db.getUserByClubPost(widget.pId),
+                      builder: ((context, snapshot) {
+                        if (snapshot.hasData) {
+                          final UserResponse user =
+                              snapshot.data as UserResponse;
+                          return Container(
+                            margin: const EdgeInsets.fromLTRB(
+                                20.0, 10.0, 20.0, 0.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      width: 41,
+                                      height: 41,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: AppTheme.mainTextColor,
+                                        image: const DecorationImage(
+                                          image: AssetImage(
+                                              'assets/images/sunset.jpg'),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10.0),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          user.nickname,
+                                          style: TextStyle(
+                                            fontSize: 12.0,
+                                            color: AppTheme.mainTextColor,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              '${user.major}  /  ',
+                                              style: TextStyle(
+                                                fontSize: 12.0,
+                                                color: AppTheme.mainTextColor,
+                                                fontWeight: FontWeight.w400,
+                                              ),
+                                            ),
+                                            Text(
+                                              user.gender,
+                                              style: TextStyle(
+                                                fontSize: 12.0,
+                                                color: AppTheme.mainTextColor,
+                                                fontWeight: FontWeight.w400,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 30.0),
+                                Text(
+                                  post.title,
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: AppTheme.mainTextColor,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                const SizedBox(height: 10.0),
+                                Divider(color: AppTheme.mainTextColor),
+                                const SizedBox(height: 10.0),
+                                Container(
+                                  margin: const EdgeInsets.only(bottom: 10.0),
+                                  width: double.infinity,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const SizedBox(height: 20.0),
+                                      Text(
+                                        post.content,
+                                        style: TextStyle(
+                                          color: AppTheme.mainTextColor,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                        maxLines: 20,
+                                      ),
+                                      const SizedBox(height: 20.0),
+                                    ],
+                                  ),
+                                ),
+                                Divider(color: AppTheme.mainTextColor),
+                                commentList(widget.pId, widget.type),
+                              ],
+                            ),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Center(
+                            child: Text("${snapshot.error}"),
+                          );
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text("${snapshot.error}"),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
             ],
-          ),
+          ]),
         ),
         bottomNavigationBar: Container(
             // 밑에 고정된 버튼
