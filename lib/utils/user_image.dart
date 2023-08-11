2641e3002d0f0731d1c8768b317b2e480d6ef3dd
mi-fasol <minyoong39@naver.com>
2023-08-11 19:56:02 +0900

    feat: user_image 리스트 생성

---
+++ lib/utils/user_image.dart
@@ -0,0 +1,10 @@
+List<String> userImage = [
+  "assets/user/user_dog.png",
+  "assets/user/user_peng.png",
+  "assets/user/user_rabbit.png",
+  "assets/user/user_quokka.png",
+  "assets/user/user_panda.png",
+  "assets/user/user_cat.png",
+  "assets/user/user_chick.png",
+  "assets/user/user_neogul.png",
+];
