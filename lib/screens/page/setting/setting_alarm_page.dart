53dca90ab0f2930e0fe57a2b88a6255a85611c32
seoyeon <seoyeonn051@gmail.com>
2023-08-18 00:25:04 +0900

    fix: setting_alarm_page.dart ui 완성

---
+++ lib/screens/page/setting/setting_alarm_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hae_mo/controller/setting_alarm_controller.dart';
+
+import '../../../common/color.dart';
+import '../../../common/theme.dart';
+import '../../components/customAppBar.dart';
+
+class SettingAlarmPage extends StatefulWidget {
+  const SettingAlarmPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingAlarmPage> createState() => _SettingAlarmPageState();
+}
+
+List<String> alarmList = ['게시물', '댓글', '채팅'];
+
+class _SettingAlarmPageState extends State<SettingAlarmPage> {
+  SettingAlarmController settingAlarmController = SettingAlarmController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(kToolbarHeight),
+            child: Builder(
+                builder: (context) => customColorAppbar(context, "알림 설정"))),
+        body: Container(
+            decoration: BoxDecoration(color: Color(0XFFF4F4F4)),
+            child: Column(
+              children: [
+                switchMenuItem(context, "푸시 알림 받기",-1, {}),
+                alarmMenuTitle(context, "알람"),
+                for (var i =0;i<3;i++ )
+                  switchMenuItem(context, alarmList[i], i, {})
+              ],
+            )));
+  }
+
+  Widget switchMenuItem(BuildContext context, String text, int index, void onClick) {
+    return Container(
+        //color: Color(0XFFF4F4F4),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        height: MediaQuery.of(context).size.height / 18,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            color: AppTheme.buttonColor,
+            border: Border(
+                bottom: BorderSide(
+                    color: AppTheme.settingPageDividerColor, width: 3.0))),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(index<0?"푸시 알림 받기": "${alarmList[index!]} 알림", style: CustomThemes.settingPageMenuListTextStyle),
+          CupertinoSwitch(
+            value: settingAlarmController.isCheckedList[index+1],
+            activeColor: AppTheme.mainColor,
+            onChanged: (bool? value) {
+              setState(() {
+                settingAlarmController.isCheckedList[index+1] = value ?? false;
+                onClick;
+              });
+            },
+          )
+        ]));
+  }
+}
+
+Widget alarmMenuTitle(BuildContext context, String text) {
+  return Container(
+      padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+      height: MediaQuery.of(context).size.height / 17,
+      width: MediaQuery.of(context).size.width,
+      color: AppTheme.settingPageDividerColor,
+      child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(text, style: CustomThemes.settingPageMenuTextStyle)));
+}
