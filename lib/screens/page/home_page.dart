7e71d74fce9280e76bf088b787cfe02ea03035f8
mi-fasol <minyoong39@naver.com>
2023-08-20 22:56:31 +0900

    feat: 홈페이지로 이동 시 프리퍼런스에 uId 받아와서 저장

--- lib/screens/page/home_page.dart
+++ lib/screens/page/home_page.dart
@@ -5,8 +5,10 @@ import 'package:hae_mo/common/color.dart';
 import 'package:hae_mo/controller/club_page_controller.dart';
 import 'package:hae_mo/controller/hotplace_page_controller.dart';
 import 'package:hae_mo/model/post_type.dart';
+import 'package:hae_mo/model/user_response_model.dart';
 import 'package:hae_mo/screens/page/posting_page.dart';
 import '../../controller/meeting_page_controller.dart';
+import '../../service/db_service.dart';
 import '../../utils/shared_preference.dart';
 import 'club_page.dart';
 import 'hot_place_page.dart';
@@ -41,83 +43,102 @@ class _HomePageState extends State<HomePage> {
     Get.put(MeetingPageController());
     Get.put(ClubPageController());
     Get.put(HotPlacePageController());
-    return Scaffold(
-        body: SafeArea(
-          child: _widgetOptions.elementAt(_selectedIndex),
-        ),
-        bottomNavigationBar: Container(
-          margin: const EdgeInsets.only(left: 2.0, right: 2.0),
-          decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(15.0),
-                  topLeft: Radius.circular(15.0)),
-              boxShadow: [
-                BoxShadow(
-                    color: AppTheme.mainColor,
-                    spreadRadius: 2.0,
-                    blurRadius: 0.0)
-              ]),
-          child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(15.0),
-                  topLeft: Radius.circular(15.0)),
-              child: BottomNavigationBar(
-                backgroundColor: Colors.white,
-                type: BottomNavigationBarType.fixed,
-                items: <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                      icon: Image.asset(
-                          "assets/icons/postingpage_bottomnav_icon.png"),
-                      label: "",
-                      activeIcon: Image.asset(
-                          "assets/icons/postingpage_bottomnav_icon.png",
-                          color: AppTheme.mainColor)),
-                  BottomNavigationBarItem(
-                      icon: Image.asset(
-                          "assets/icons/clubpage_bottomnav_icon.png"),
-                      label: "",
-                      activeIcon: Image.asset(
-                          "assets/icons/clubpage_bottomnav_icon.png",
-                          color: AppTheme.mainColor)),
-                  BottomNavigationBarItem(
-                      icon: Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.asset(
-                              "assets/icons/hotplace_bottomnav_icon.png")),
-                      label: "",
-                      activeIcon: Container(
-                          width: 30,
-                          height: 30,
-                          child: Image.asset(
-                              "assets/icons/hotplace_bottomnav_icon.png",
-                              color: AppTheme.mainColor))),
-                  BottomNavigationBarItem(
-                      icon: Container(
-                        width: 35.0,
-                        height: 35.0,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
+    DBService db = DBService();
+    return FutureBuilder(
+        future: db
+            .getUserByNickname(PreferenceUtil.getString("nickname").toString()),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final UserResponse user = snapshot.data as UserResponse;
+            PreferenceUtil.setInt("uId", user.uId);
+            return Scaffold(
+                body: SafeArea(
+                  child: _widgetOptions.elementAt(_selectedIndex),
+                ),
+                bottomNavigationBar: Container(
+                  margin: const EdgeInsets.only(left: 2.0, right: 2.0),
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(15.0),
+                          topLeft: Radius.circular(15.0)),
+                      boxShadow: [
+                        BoxShadow(
                             color: AppTheme.mainColor,
-                            width: 0.8,
-                          ),
-                          color: Colors.transparent,
-                          image: const DecorationImage(
-                            ///임시
-                            image: AssetImage('assets/user/user_cat.png'),
-                          ),
-                        ),
-                      ),
-                      label: ""),
-                ],
-                currentIndex: _selectedIndex,
-                unselectedItemColor: const Color(0xffadadad),
-                selectedItemColor: AppTheme.mainColor,
-                onTap: _onItemTapped,
-              )),
-        ),
-        floatingActionButton: _selectedIndex != 3 ? floatingButton() : null);
+                            spreadRadius: 2.0,
+                            blurRadius: 0.0)
+                      ]),
+                  child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(15.0),
+                          topLeft: Radius.circular(15.0)),
+                      child: BottomNavigationBar(
+                        backgroundColor: Colors.white,
+                        type: BottomNavigationBarType.fixed,
+                        items: <BottomNavigationBarItem>[
+                          BottomNavigationBarItem(
+                              icon: Image.asset(
+                                  "assets/icons/postingpage_bottomnav_icon.png"),
+                              label: "",
+                              activeIcon: Image.asset(
+                                  "assets/icons/postingpage_bottomnav_icon.png",
+                                  color: AppTheme.mainColor)),
+                          BottomNavigationBarItem(
+                              icon: Image.asset(
+                                  "assets/icons/clubpage_bottomnav_icon.png"),
+                              label: "",
+                              activeIcon: Image.asset(
+                                  "assets/icons/clubpage_bottomnav_icon.png",
+                                  color: AppTheme.mainColor)),
+                          BottomNavigationBarItem(
+                              icon: Container(
+                                  width: 30,
+                                  height: 30,
+                                  child: Image.asset(
+                                      "assets/icons/hotplace_bottomnav_icon.png")),
+                              label: "",
+                              activeIcon: Container(
+                                  width: 30,
+                                  height: 30,
+                                  child: Image.asset(
+                                      "assets/icons/hotplace_bottomnav_icon.png",
+                                      color: AppTheme.mainColor))),
+                          BottomNavigationBarItem(
+                              icon: Container(
+                                width: 35.0,
+                                height: 35.0,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: AppTheme.mainColor,
+                                    width: 0.8,
+                                  ),
+                                  color: Colors.transparent,
+                                  image: const DecorationImage(
+                                    ///임시
+                                    image:
+                                        AssetImage('assets/user/user_cat.png'),
+                                  ),
+                                ),
+                              ),
+                              label: ""),
+                        ],
+                        currentIndex: _selectedIndex,
+                        unselectedItemColor: const Color(0xffadadad),
+                        selectedItemColor: AppTheme.mainColor,
+                        onTap: _onItemTapped,
+                      )),
+                ),
+                floatingActionButton:
+                    _selectedIndex != 3 ? floatingButton() : null);
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("${snapshot.error}"),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 
   @override
