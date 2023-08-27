ea3de2d38dc36079d5cf33fe6e753967828dd62c
mi-fasol <minyoong39@naver.com>
2023-08-27 21:58:25 +0900

    fix: 신고하기 페이지 키보드 올라와도 화면 안 올라가는 오류 수정

--- lib/screens/components/customButton.dart
+++ lib/screens/components/customButton.dart
@@ -1,39 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:hae_mo/controller/image_controller.dart';
 import 'package:hae_mo/controller/posting_controller.dart';
+import 'package:hae_mo/screens/page/home_page.dart';
 
 import '../../common/color.dart';
 import '../../common/theme.dart';
 
-
 /// PostingPage 등록 버튼
 Widget postingButton(BuildContext context, void Function()? onPressed) {
-  return ElevatedButton(onPressed: onPressed,
+  return ElevatedButton(
+      onPressed: onPressed,
       style: OutlinedButton.styleFrom(backgroundColor: AppTheme.mainColor),
-      child: Text("등록하기", style: TextStyle(
-        fontFamily: Theme.of(context).textTheme.headlineSmall?.fontFamily,
-          fontSize: Theme.of(context).textTheme.headlineSmall?.fontSize,
-
-          color: AppTheme.postingPageHeadlineColor)));
+      child: Text("등록하기",
+          style: TextStyle(
+              fontFamily: Theme.of(context).textTheme.headlineSmall?.fontFamily,
+              fontSize: Theme.of(context).textTheme.headlineSmall?.fontSize,
+              color: AppTheme.postingPageHeadlineColor)));
 }
 
-
-Widget settingPageCustomButton(String content, Function onClick){
+Widget settingPageCustomButton(String content, Function onClick) {
   return GestureDetector(
-      onTap: () {
-        onClick();
-      },
-      child: Container(
-          height: 45,
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          decoration: BoxDecoration(
-              color: AppTheme.mainColor,
-              borderRadius: BorderRadius.circular(50)),
-          child: Center(
-            child: Text(content,
-                style:
-                CustomThemes.deleteAccountPageButtonTextStyle),
-          )),
-
+    onTap: () {
+      onClick();
+    },
+    child: Container(
+        height: 45,
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+            color: AppTheme.mainColor, borderRadius: BorderRadius.circular(50)),
+        child: Center(
+          child: Text(content,
+              style: CustomThemes.deleteAccountPageButtonTextStyle),
+        )),
   );
+}
+
+Widget reportingPageCustomBottn(String content) {
+  return Container(
+      height: 42.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+          color: AppTheme.mainColor),
+      child: RawMaterialButton(
+          onPressed: (() {
+            HomePage homePage = HomePage();
+            Get.offAll(() => homePage);
+          }),
+          child: const Text(
+            "신고하기",
+            style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w500,
+                color: Colors.white),
+          )));
 }
