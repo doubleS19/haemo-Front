e134bd9a130397a9f028e94b6818905338acd84e
mi-fasol <minyoong39@naver.com>
2023-08-31 23:07:51 +0900

    refactor: userProfile 위젯 분리

---
+++ lib/screens/components/userProfileWidget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hae_mo/common/color.dart';
+import 'package:hae_mo/model/user_response_model.dart';
+import 'package:hae_mo/screens/components/userBottomSheet.dart';
+
+Row userProfile(BuildContext context, UserResponse user) {
+  return Row(
+    children: [
+      SizedBox(
+          width: 41.0,
+          height: 41.0,
+          child: RawMaterialButton(
+              elevation: 0.0,
+              fillColor: Colors.transparent,
+              shape: CircleBorder(),
+              onPressed: (() {
+                userBottomSheet(context, user);
+              }),
+              child: Container(
+                width: 41,
+                height: 41,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.transparent,
+                  image: DecorationImage(
+                    image: AssetImage(user.userImage),
+                  ),
+                ),
+              ))),
+      const SizedBox(width: 10.0),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            user.nickname,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: AppTheme.mainTextColor,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Row(
+            children: [
+              Text(
+                '${user.major}  /  ',
+                style: TextStyle(
+                  fontSize: 12.0,
+                  color: AppTheme.mainTextColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                user.gender,
+                style: TextStyle(
+                  fontSize: 12.0,
+                  color: AppTheme.mainTextColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
