2b9e3d054d62b4a1cdc9be844a1411e14be35ace
mi-fasol <minyoong39@naver.com>
2023-09-05 19:23:36 +0900

    feat: wishClub 관련 api 추가

--- lib/service/db_service.dart
+++ lib/service/db_service.dart
@@ -588,4 +588,61 @@ class DBService {
       throw Exception('Failed to load post list');
     }
   }
+
+  Future<bool> checkWishClubExist(int uId, int pId) async {
+    final response = await http
+        .get(Uri.parse("http://43.201.211.1:1004/wishClub/isExist/$uId/$pId"));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as bool;
+      return data;
+    } else {
+      throw Exception('Failed to check wish Meeting exist.');
+    }
+  }
+
+  Future<bool> addWishClubList(WishMeeting wish) async {
+    try {
+      final response = await http.post(
+        Uri.parse("http://43.201.211.1:1004/wishClub"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(wish.toJson()),
+      );
+      if (response.statusCode != 201) {
+        throw Exception("Failed to send data");
+      } else {
+        dev.log("Wish Meeting List Data sent successfully");
+        return true;
+      }
+    } catch (e) {
+      dev.log("Failed to send Wish Meeting List: ${e}");
+      return false;
+    }
+  }
+
+  Future<void> deleteWishClubList(int uId, int pId) async {
+    final response = await http.delete(
+      Uri.parse('http://43.201.211.1:1004/wishClub/delete/$uId/$pId'),
+    );
+    if (response.statusCode == 204) {
+      print('WishList deleted successfully');
+    } else {
+      throw Exception('Failed to delete Wish Meeting List');
+    }
+  }
+
+  Future<List<ClubPostResponse>> getWishClubListByUser(int uId) async {
+    final response = await http
+        .get(Uri.parse('http://43.201.211.1:1004/wishClub/myList/$uId'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List<dynamic>;
+      return data
+          .map<ClubPostResponse>((json) => ClubPostResponse.fromJson(json))
+          .toList();
+    } else {
+      throw Exception('Failed to load post list');
+    }
+  }
 }
