702e2513b99404e00ea4e600f70d92e21f443638
seoyeon <seoyeonn051@gmail.com>
2023-09-14 04:00:56 +0900

    fix: 존재하는 chatList 확인 기능 추가

--- lib/controller/chatlist_controller.dart
+++ lib/controller/chatlist_controller.dart
@@ -5,6 +5,7 @@ import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:hae_mo/model/chat_message_model.dart';
 import 'package:hae_mo/model/user_response_model.dart';
 import 'package:hae_mo/service/db_service.dart';
+import 'package:hae_mo/utils/shared_preference.dart';
 
 import '../screens/Page/chat/chat_list_page.dart';
 import '../model/chatlist_model.dart';
@@ -14,31 +15,47 @@ class ChatListController extends GetxController {
   DBService dbService = DBService();
   List<ChatList> chatList = [];
   StreamController streamController = StreamController();
+  late int uId;
+  late String nickname;
 
   @override
   void onInit() {
     super.onInit();
+    uId = PreferenceUtil.getInt("uid")!;
+    nickname = PreferenceUtil.getString("nickname")!;
   }
 
   void addChatList() async {
-    await firestore.collection("group").doc().set(ChatList(
-        createdAt: DateTime.now(),
-        createdBy: "seoyeon",
-        id: "id",
-        isDeleted: false,
-        membersId: [43, 47],
-        recentMessage: ChatMessage(
-          messageText: "Hi!",
-          sentBy: 43,
-          sentAt: DateTime.now(),
-          isRead: false
-        )).toJson());
+    bool isEnabled = false;
+
+    await firestore.collection("group").where("members", isEqualTo: [43, 47]).where("isDeleted", isEqualTo: true).get().then((value) {
+      print("print: ${value.docs}");
+      if(value.docs.isEmpty){
+        isEnabled = true;
+      }
+    });
+
+    if(isEnabled){
+      await firestore.collection("group").doc().set(ChatList(
+          createdAt: DateTime.now(),
+          createdBy: "seoyeon",
+          id: "id",
+          isDeleted: false,
+          membersId: [43, 47],
+          recentMessage: ChatMessage(
+              messageText: "Hi!",
+              sentBy: 43,
+              sentAt: DateTime.now(),
+              isRead: false
+          )).toJson());
+    }
   }
 
   void getChatList() async{
-    QuerySnapshot<Map<String, dynamic>> _snapshot = await firestore.collection("group").get();
-    print("들어온 데이터는? ${_snapshot.docs.map((e) => ChatList.fromJson(e.data())).toList()}");
-    chatList = _snapshot.docs.map((e) => ChatList.fromJson(e.data())).toList();
+      QuerySnapshot<Map<String, dynamic>> _snapshot = await firestore.collection("group").get();
+      print("들어온 데이터는? ${_snapshot.docs.map((e) => ChatList.fromJson(e.data())).toList()}");
+      chatList = _snapshot.docs.map((e) => ChatList.fromJson(e.data())).toList();
+
   }
 
   Future<UserResponse> getUserNicknameById(int uId) async{
