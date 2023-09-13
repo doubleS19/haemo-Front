5ab4ce9187b01a205cee4761248b4e2a66dc8098
seoyeon <seoyeonn051@gmail.com>
2023-09-14 01:29:58 +0900

    fix: chatList page UI 수정

--- lib/screens/page/chat/chat_list_page.dart
+++ lib/screens/page/chat/chat_list_page.dart
@@ -1,10 +1,11 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:hae_mo/model/chatlist_model.dart';
+import 'package:hae_mo/model/user_response_model.dart';
+import 'package:hae_mo/service/db_service.dart';
 import '../../../controller/chatlist_controller.dart';
 import '../../../model/chat_message_model.dart';
+import '../../components/userBottomSheet.dart';
 
 class ChatListPage extends StatefulWidget {
   const ChatListPage({super.key});
@@ -35,62 +36,110 @@ class _ChatListPageState extends State<ChatListPage> {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body:
-          Container(
-            height: MediaQuery.sizeOf(context).height,
-            width: MediaQuery.sizeOf(context).width,
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Container(
-                  height: 500,
-                  width: MediaQuery.sizeOf(context).width,
-                  child: ListView.builder(
-                    itemCount: chatListController.chatList.length,
+      body: Container(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height*0.8,
+                width: MediaQuery.sizeOf(context).width,
+                child: ListView.builder(
+                    itemCount: 3,
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                          height: 100,
-                          width: MediaQuery.of(context).size.width,
-                          child: Slidable(
-                            endActionPane: ActionPane(
-                              extentRatio: 0.3,
-                              motion: const DrawerMotion(), // 스와이프 애니메이션..
-                              children: [
-                                SlidableAction(
-                                  flex: 1,
-                                  // 여러 액션이 있을때 차지하는 비율
-                                  onPressed: (BuildContext context) => doNothing(context),
-                                  backgroundColor: Color.fromARGB(196, 172, 49, 38),
-                                  foregroundColor: Colors.white,
-                                  icon: Icons.check_rounded,
-                                  label: 'CHECK',
-                                ),
-                              ],
-                            ),
-                            child: chatCard(chatListController.chatList[index].recentMessage),
-                          )
+                      return Column(
+                        children: [
+                          slidableCard(),
+                          const Divider(thickness: 1)
+                        ],
                       );
-                    }
-                  ),
-                ),
-                TextButton(
-                    onPressed: () {
-                      chatListController.addChatList();
-                      print("click addChatList");
-                    },
-                    child: Text("click to make List"))
-              ],
-            )
-          ),
+                    }),
+              ),
+              TextButton(
+                  onPressed: () {
+                    chatListController.addChatList();
+                    print("click addChatList");
+                  },
+                  child: Text("click to make List"))
+            ],
+          )),
     );
   }
+
+  UserResponse user = UserResponse(
+      uId: 43,
+      studentId: "studentId",
+      nickname: "nickname",
+      major: "major",
+      userImage: "rabbit",
+      gender: "");
+
   void doNothing(BuildContext context) {}
+
+  Widget slidableCard() {
+    return Container(
+        height: 80,
+        //width: MediaQuery.of(context).size.width,
+        child: Slidable(
+              endActionPane: ActionPane(
+                extentRatio: 0.3,
+                motion: const DrawerMotion(),
+                children: [
+                  SlidableAction(
+                    flex: 1,
+                    onPressed: (BuildContext context) => doNothing(context),
+                    backgroundColor: Color.fromARGB(196, 172, 49, 38),
+                    foregroundColor: Colors.white,
+                    icon: Icons.check_rounded,
+                    label: 'CHECK',
+                  ),
+                ],
+              ),
+              child: chatCard(ChatMessage(
+                  messageText: "text",
+                  sentBy: 43,
+                  sentAt: DateTime.now(),
+                  isRead: false)),
+            ));
+  }
+
+  Widget chatCard(ChatMessage chat) {
+    return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            profileImage(context, user),
+            Container(
+              width: MediaQuery.sizeOf(context).width*0.7,
+              child: ListTile(
+                title: Text(user.nickname),
+                subtitle: Text(chat.messageText ?? ""),
+              ))]);
+  }
 }
-Widget chatCard(ChatMessage chat) {
-  return Container(
-    child: ListTile(
-      title: Text(chat.sentBy),
-      subtitle: Text(chat.messageText ?? ""),
-    ),
-  );
+
+
+Widget profileImage(BuildContext context, UserResponse user){
+  return SizedBox(
+      width: 41.0,
+      height: 41.0,
+      child: RawMaterialButton(
+          elevation: 0.0,
+          fillColor: Colors.transparent,
+          shape: CircleBorder(),
+          onPressed: (() {
+            userBottomSheet(context, user);
+          }),
+          child: Container(
+            width: 41,
+            height: 41,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              image: DecorationImage(
+                image: AssetImage(
+                    "assets/user/user_dog.png"),
+              ),
+            ),
+          )));
 }
