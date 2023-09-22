e76b56172d08bf86ce6271b5d925db619d27a604
seoyeon <seoyeonn051@gmail.com>
2023-09-22 15:25:59 +0900

    fix: chatList 페이지 slidableCard 분리 및 FutureBuilder 적용

--- lib/controller/chatlist_controller.dart
+++ lib/controller/chatlist_controller.dart
@@ -1,14 +1,10 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-import 'package:hae_mo/model/chat_message_model.dart';
 import 'package:hae_mo/model/user_response_model.dart';
 import 'package:hae_mo/service/db_service.dart';
 import 'package:hae_mo/utils/shared_preference.dart';
-
-import '../screens/Page/chat/chat_list_page.dart';
 import '../model/chatroom_model.dart';
 
 class ChatListController extends GetxController {
@@ -17,34 +13,30 @@ class ChatListController extends GetxController {
   RxList chatList = [].obs;
   StreamController streamController = StreamController();
   late int uId;
-  late String nickname;
 
-  @override
-  void onInit() {
-    super.onInit();
-    nickname = PreferenceUtil.getString("nickname")!;
+  void getChatList() async {
+    print("uid: $uId");
+    QuerySnapshot<Map<String, dynamic>> _snapshot = await firestore
+        .collection("group")
+        .where("membersId", arrayContains: uId)
+        .where("isDeleted", isEqualTo: false)
+        .get();
+    print(
+        "들어온 데이터는? ${_snapshot.docs.map((e) => ChatRoom.fromJson(e.data())).toList()}");
+    chatList.value =
+        _snapshot.docs.map((e) => ChatRoom.fromJson(e.data())).toList();
   }
 
-  void getChatList() async{
-      print("uid: $uId");
-      QuerySnapshot<Map<String, dynamic>> _snapshot = await firestore.collection("group").where("membersId",arrayContains: uId).where("isDeleted", isEqualTo: false).get();
-      print("들어온 데이터는? ${_snapshot.docs.map((e) => ChatRoom.fromJson(e.data())).toList()}");
-      chatList.value = _snapshot.docs.map((e) => ChatRoom.fromJson(e.data())).toList();
-  }
-
-  int getOtherId(ChatRoom chatRoomData){
-    if(uId == chatRoomData.membersId[0]){
-      return chatRoomData.membersId[1];
+  Future<UserResponse> getOtherUserInfo(ChatRoom chatRoomData) async {
+    int otherUid;
+    if (uId == chatRoomData.membersId[0]) {
+      otherUid = chatRoomData.membersId[1];
     }
-    return chatRoomData.membersId[0];
-  }
-
-  Future<UserResponse> getUserNicknameById(int uId) async{
-    UserResponse user = await dbService.getUserById(uId);
-    return user;
+    otherUid = chatRoomData.membersId[0];
+    return await dbService.getUserById(otherUid);
   }
 
   void deleteChatList(String chatId) {
-    firestore.collection("group").doc(chatId).update({"isDeleted":true});
+    firestore.collection("group").doc(chatId).update({"isDeleted": true});
   }
 }
--- lib/screens/page/chat/chat_list_page.dart
+++ lib/screens/page/chat/chat_list_page.dart
@@ -3,8 +3,10 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:hae_mo/controller/chat_controller.dart';
 import 'package:hae_mo/model/user_response_model.dart';
 import 'package:hae_mo/screens/page/chat/chat_room_page.dart';
+import 'package:hae_mo/service/db_service.dart';
 import '../../../common/theme.dart';
 import '../../../controller/chatlist_controller.dart';
 import '../../../model/chat_message_model.dart';
@@ -38,7 +40,7 @@ class _ChatListPageState extends State<ChatListPage> {
           preferredSize: const Size.fromHeight(kToolbarHeight),
           child: Builder(builder: (context) => backButtonAppbar(context, ""))),
       body: Container(
-          height: MediaQuery.sizeOf(context).height*0.9,
+          height: MediaQuery.sizeOf(context).height * 0.9,
           width: MediaQuery.sizeOf(context).width,
           padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
           child: Column(
@@ -52,62 +54,100 @@ class _ChatListPageState extends State<ChatListPage> {
               SizedBox(
                   height: MediaQuery.sizeOf(context).height * 0.7, // 0.75
                   width: MediaQuery.sizeOf(context).width,
-                  child: Obx(()=> chatListController.chatList.isNotEmpty? ListView.separated(
-                      padding: EdgeInsets.zero,
-                      itemCount: chatListController.chatList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return slidableCard(chatListController.chatList[index], index);
-                      },
-                      separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(height: 1)):Container()))
+                  child: Obx(() => chatListController.chatList.isNotEmpty
+                      ? ListView.separated(
+                          padding: EdgeInsets.zero,
+                          itemCount: chatListController.chatList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return SlidableCard(
+                              chat: chatListController.chatList[index],
+                              index: index,
+                              chatListController: chatListController,
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(height: 1))
+                      : Container()))
             ],
           )),
     );
   }
+}
 
-  UserResponse user = UserResponse(
-      uId: 43,
-      studentId: "studentId",
-      nickname: "nickname",
-      major: "major",
-      userImage: "rabbit",
-      gender: "");
+class SlidableCard extends StatefulWidget {
+  const SlidableCard(
+      {Key? key,
+      required this.chat,
+      required this.index,
+      required this.chatListController})
+      : super(key: key);
 
-  Widget slidableCard(ChatRoom chat, int index) {
-    return GestureDetector(
-        onTap: () {
-          Get.to(() => ChatRoomPage(chatRoomId: chat.id, otherUserId: chatListController.getOtherId(chat)));
-        },
-      child: Container(
-          height: 85,
-          child: Slidable(
-            endActionPane: ActionPane(
-              extentRatio: 0.3,
-              motion: const DrawerMotion(),
-              children: [
-                SlidableAction(
-                  flex: 1,
-                  onPressed: (BuildContext context) {
-                    chatListController.deleteChatList(chat.id);
-                    chatListController.chatList.removeAt(index);
-                  },
-                  backgroundColor: Color.fromARGB(196, 172, 49, 38),
-                  foregroundColor: Colors.white,
-                  icon: Icons.check_rounded,
-                  label: 'CHECK',
-                ),
-              ],
-            ),
-            child: chatCard(chat.recentMessage),
-          ))
-    );
+  final ChatRoom chat;
+  final int index;
+  final ChatListController chatListController;
+
+  @override
+  State<SlidableCard> createState() => _SlidableCardState();
+}
+
+class _SlidableCardState extends State<SlidableCard> {
+  late ChatListController chatListController;
+  late final UserResponse user;
+  late int uId;
+
+  @override
+  void initState() {
+    super.initState();
+    chatListController = widget.chatListController;
+    chatListController.getOtherUserInfo(widget.chat);
   }
 
-  Widget chatCard(ChatMessage chat) {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: chatListController.getOtherUserInfo(widget.chat),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            return GestureDetector(
+                onTap: () {
+                  Get.to(() =>
+                      ChatRoomPage(chatRoomId: widget.chat.id, otherUser: snapshot.data!));
+                },
+                child: SizedBox(
+                    height: 85,
+                    child: Slidable(
+                      endActionPane: ActionPane(
+                        extentRatio: 0.3,
+                        motion: const DrawerMotion(),
+                        children: [
+                          SlidableAction(
+                            flex: 1,
+                            onPressed: (BuildContext context) {
+                              widget.chatListController
+                                  .deleteChatList(widget.chat.id);
+                              widget.chatListController.chatList
+                                  .removeAt(widget.index);
+                            },
+                            backgroundColor: Color.fromARGB(196, 172, 49, 38),
+                            foregroundColor: Colors.white,
+                            icon: Icons.check_rounded,
+                            label: 'CHECK',
+                          ),
+                        ],
+                      ),
+                      child: chatCard(widget.chat.recentMessage, snapshot.data!),
+                    )));
+          }else{
+            return Container();
+          }
+        });
+  }
+
+  Widget chatCard(ChatMessage chat,UserResponse user) {
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-          profileImage(context, user),
+          profileImage(context, user!),
           Container(
               height: double.maxFinite,
               width: MediaQuery.sizeOf(context).width * 0.7,
@@ -117,28 +157,28 @@ class _ChatListPageState extends State<ChatListPage> {
               ))
         ]));
   }
-}
 
-Widget profileImage(BuildContext context, UserResponse user) {
-  return SizedBox(
-      width: MediaQuery.sizeOf(context).width * 0.15,
-      height: MediaQuery.sizeOf(context).width * 0.15,
-      child: RawMaterialButton(
-          elevation: 0.0,
-          fillColor: Colors.transparent,
-          shape: CircleBorder(),
-          onPressed: (() {
-            userBottomSheet(context, user);
-          }),
-          child: Container(
-            width: MediaQuery.sizeOf(context).width * 0.15,
-            height: MediaQuery.sizeOf(context).width * 0.15,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.transparent,
-              image: DecorationImage(
-                image: AssetImage("assets/user/user_dog.png"),
+  Widget profileImage(BuildContext context, UserResponse user) {
+    return SizedBox(
+        width: MediaQuery.sizeOf(context).width * 0.15,
+        height: MediaQuery.sizeOf(context).width * 0.15,
+        child: RawMaterialButton(
+            elevation: 0.0,
+            fillColor: Colors.transparent,
+            shape: CircleBorder(),
+            onPressed: (() {
+              userBottomSheet(context, user);
+            }),
+            child: Container(
+              width: MediaQuery.sizeOf(context).width * 0.15,
+              height: MediaQuery.sizeOf(context).width * 0.15,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                image: DecorationImage(
+                  image: AssetImage("assets/user/user_dog.png"),
+                ),
               ),
-            ),
-          )));
+            )));
+  }
 }
