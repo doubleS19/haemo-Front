fb9697cc58bfd169287cdc91f36dcd95470a0779
mi-fasol <minyoong39@naver.com>
2023-09-26 03:20:19 +0900

    feat: user_round image 추가

---
+++ lib/common/user_image.dart
@@ -0,0 +1,21 @@
+var userImage = [
+  "assets/user/user_cat.png",
+  "assets/user/user_chick.png",
+  "assets/user/user_dog.png",
+  "assets/user/user_neogul.png",
+  "assets/user/user_panda.png",
+  "assets/user/user_peng.png",
+  "assets/user/user_quokka.png",
+  "assets/user/user_rabbit.png"
+];
+
+var userRoundImage = [
+  "assets/user/round_cat.png",
+  "assets/user/round_chick.png",
+  "assets/user/round_dog.png",
+  "assets/user/round_neogul.png",
+  "assets/user/round_panda.png",
+  "assets/user/round_peng.png",
+  "assets/user/round_quokka.png",
+  "assets/user/round_rabbit.png"
+];
