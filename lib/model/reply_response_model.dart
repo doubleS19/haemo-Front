b40689a67c96b3d53a1b5dddd9bba672fa0a6dea
mi-fasol <minyoong39@naver.com>
2023-10-16 14:24:21 +0900

    fix: reply 모델 하나로 통합

--- lib/model/reply_response_model.dart
+++ lib/model/reply_response_model.dart
@@ -1,31 +1,46 @@
+import 'package:hae_mo/model/reply_model.dart';
+
+enum ReplyType { Post, Club, HotPlace }
+
 class ReplyResponse {
   final String content;
   final String nickname;
   final String date;
   final int rId;
   final int cId;
+  final ReplyType type;
 
   ReplyResponse(
       {required this.content,
       required this.nickname,
       required this.date,
       required this.cId,
-      required this.rId});
+      required this.rId,
+      required this.type});
 
-  factory ReplyResponse.fromJson(Map<String, dynamic> json) {
+  factory ReplyResponse.fromJson(Map<String, dynamic> json, ReplyType type) {
     return ReplyResponse(
         content: json['content'],
         nickname: json['nickname'],
         date: json['date'],
-        cId: json['cid'],
-        rId: json['rid']);
+        cId: json[type == ReplyType.Post
+            ? 'cid'
+            : (type == ReplyType.Club ? 'ccId' : 'hcId')],
+        rId: json[type == ReplyType.Post
+            ? 'rid'
+            : (type == ReplyType.Club ? 'crId' : 'hrId')],
+        type: type);
   }
 
   Map<String, dynamic> toJson() => {
         'content': content,
         'nickname': nickname,
         'date': date,
-        'cid': cId,
-        'rid': rId
+        if (type == ReplyType.Post) 'cid': cId,
+        if (type == ReplyType.Post) 'rid': rId,
+        if (type == ReplyType.Club) 'ccId': cId,
+        if (type == ReplyType.Club) 'crId': rId,
+        if (type == ReplyType.Post) 'hcId': cId,
+        if (type == ReplyType.Post) 'hrId': cId,
       };
 }
--- lib/screens/components/replyWidget.dart
+++ lib/screens/components/replyWidget.dart
@@ -8,179 +8,19 @@ import 'dart:developer' as dev;
 
 Widget replyWidget(int cId, int type) {
   DBService db = DBService();
-  if (type == 1) {
-    return FutureBuilder<List<ReplyResponse>>(
-        future: db.getReplysByCId(cId),
-        builder: (context, snapshot) {
-          print(snapshot.data);
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // 데이터 로딩
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            dev.log(snapshot.error.toString());
-            // 오류
-            return const Center(
-              child: Text("댓글을 불러오는 중 오류가 발생했습니다."),
-            );
-          } else if (snapshot.hasData && snapshot.data!.isEmpty) {
-            // 댓글이 없는 경우 빈 컨테이너 반환
-            return Container();
-          } else {
-            // 댓글 목록
-            final replyList = snapshot.data!;
-            return ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: replyList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    children: [
-                      Container(
-                        height: 50.0,
-                        width: double.infinity,
-                        margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
-                        child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Row(children: [
-                              const SizedBox(width: 20.0),
-                              SizedBox(
-                                width: 15.0,
-                                height: 15.0,
-                                child:
-                                    Image.asset("assets/icons/reply_icon.png"),
-                              ),
-                              const SizedBox(width: 10.0),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    replyList[index].nickname,
-                                    style: TextStyle(
-                                      color: AppTheme.mainTextColor,
-                                      fontSize: 12.0,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      children: [
-                                        Text(
-                                          replyList[index].content,
-                                          style: TextStyle(
-                                              fontSize: 12.0,
-                                              color: AppTheme.mainTextColor,
-                                              fontWeight: FontWeight.w500),
-                                        ),
-                                        const SizedBox(width: 5.0),
-                                        // Align(alignment: Alignment., child:
-                                        Text(
-                                          replyList[index].date,
+  Future<List<ReplyResponse>>? replyFuture;
 
-                                          style: TextStyle(
-                                              fontSize: 8.0,
-                                              color: AppTheme.mainTextColor),
-                                          // )
-                                        )
-                                      ])
-                                ],
-                              ),
-                            ])),
-                      ),
-                    ],
-                  );
-                });
-          }
-        });
+  if (type == 1) {
+    replyFuture = db.getReplysByCId(cId);
   } else if (type == 2) {
-    return FutureBuilder<List<ClubReplyResponse>>(
-      future: db.getClubReplysByCcId(cId),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          // 데이터 로딩
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.hasError) {
-          // 오류
-          return const Center(
-            child: Text("댓글을 불러오는 중 오류가 발생했습니다."),
-          );
-        } else if (snapshot.hasData && snapshot.data!.isEmpty) {
-          // 댓글이 없는 경우
-          return Container();
-        } else {
-          // 댓글 목록
-          final replyList = snapshot.data!;
-          return ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: replyList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    Container(
-                      height: 50.0,
-                      width: double.infinity,
-                      margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Row(children: [
-                            const SizedBox(width: 20.0),
-                            SizedBox(
-                              width: 15.0,
-                              height: 15.0,
-                              child: Image.asset("assets/icons/reply_icon.png"),
-                            ),
-                            const SizedBox(width: 10.0),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  replyList[index].nickname,
-                                  style: TextStyle(
-                                    color: AppTheme.mainTextColor,
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                Row(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        replyList[index].content,
-                                        style: TextStyle(
-                                            fontSize: 12.0,
-                                            color: AppTheme.mainTextColor,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      const SizedBox(width: 5.0),
-                                      // Align(alignment: Alignment., child:
-                                      Text(
-                                        replyList[index].date,
-                                        style: TextStyle(
-                                            fontSize: 6.0,
-                                            color: AppTheme.mainTextColor),
-                                        // )
-                                      )
-                                    ])
-                              ],
-                            ),
-                          ])),
-                    ),
-                  ],
-                );
-              });
-        }
-      },
-    );
+    replyFuture = db.getClubReplysByCcId(cId);
   } else {
-    return FutureBuilder<List<HotPlaceReplyResponse>>(
-      future: db.getHotPlaceReplysByHcId(cId),
+    replyFuture = db.getHotPlaceReplysByHcId(cId);
+  }
+
+  // if (type == 1) {
+  return FutureBuilder<List<ReplyResponse>>(
+      future: db.getReplysByCId(cId),
       builder: (context, snapshot) {
         print(snapshot.data);
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -189,12 +29,13 @@ Widget replyWidget(int cId, int type) {
             child: CircularProgressIndicator(),
           );
         } else if (snapshot.hasError) {
+          dev.log(snapshot.error.toString());
           // 오류
           return const Center(
             child: Text("댓글을 불러오는 중 오류가 발생했습니다."),
           );
         } else if (snapshot.hasData && snapshot.data!.isEmpty) {
-          // 댓글이 없는 경우
+          // 댓글이 없는 경우 빈 컨테이너 반환
           return Container();
         } else {
           // 댓글 목록
@@ -246,8 +87,9 @@ Widget replyWidget(int cId, int type) {
                                       // Align(alignment: Alignment., child:
                                       Text(
                                         replyList[index].date,
+
                                         style: TextStyle(
-                                            fontSize: 6.0,
+                                            fontSize: 8.0,
                                             color: AppTheme.mainTextColor),
                                         // )
                                       )
@@ -260,7 +102,173 @@ Widget replyWidget(int cId, int type) {
                 );
               });
         }
-      },
-    );
-  }
+      });
+  // } else if (type == 2) {
+  //   return FutureBuilder<List<ClubReplyResponse>>(
+  //     future: db.getClubReplysByCcId(cId),
+  //     builder: (context, snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.waiting) {
+  //         // 데이터 로딩
+  //         return const Center(
+  //           child: CircularProgressIndicator(),
+  //         );
+  //       } else if (snapshot.hasError) {
+  //         // 오류
+  //         return const Center(
+  //           child: Text("댓글을 불러오는 중 오류가 발생했습니다."),
+  //         );
+  //       } else if (snapshot.hasData && snapshot.data!.isEmpty) {
+  //         // 댓글이 없는 경우
+  //         return Container();
+  //       } else {
+  //         // 댓글 목록
+  //         final replyList = snapshot.data!;
+  //         return ListView.builder(
+  //             shrinkWrap: true,
+  //             physics: const NeverScrollableScrollPhysics(),
+  //             itemCount: replyList.length,
+  //             itemBuilder: (BuildContext context, int index) {
+  //               return Column(
+  //                 children: [
+  //                   Container(
+  //                     height: 50.0,
+  //                     width: double.infinity,
+  //                     margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+  //                     child: Align(
+  //                         alignment: Alignment.centerLeft,
+  //                         child: Row(children: [
+  //                           const SizedBox(width: 20.0),
+  //                           SizedBox(
+  //                             width: 15.0,
+  //                             height: 15.0,
+  //                             child: Image.asset("assets/icons/reply_icon.png"),
+  //                           ),
+  //                           const SizedBox(width: 10.0),
+  //                           Column(
+  //                             crossAxisAlignment: CrossAxisAlignment.start,
+  //                             mainAxisAlignment: MainAxisAlignment.center,
+  //                             children: [
+  //                               Text(
+  //                                 replyList[index].nickname,
+  //                                 style: TextStyle(
+  //                                   color: AppTheme.mainTextColor,
+  //                                   fontSize: 12.0,
+  //                                   fontWeight: FontWeight.w600,
+  //                                 ),
+  //                               ),
+  //                               Row(
+  //                                   crossAxisAlignment: CrossAxisAlignment.end,
+  //                                   children: [
+  //                                     Text(
+  //                                       replyList[index].content,
+  //                                       style: TextStyle(
+  //                                           fontSize: 12.0,
+  //                                           color: AppTheme.mainTextColor,
+  //                                           fontWeight: FontWeight.w500),
+  //                                     ),
+  //                                     const SizedBox(width: 5.0),
+  //                                     // Align(alignment: Alignment., child:
+  //                                     Text(
+  //                                       replyList[index].date,
+  //                                       style: TextStyle(
+  //                                           fontSize: 6.0,
+  //                                           color: AppTheme.mainTextColor),
+  //                                       // )
+  //                                     )
+  //                                   ])
+  //                             ],
+  //                           ),
+  //                         ])),
+  //                   ),
+  //                 ],
+  //               );
+  //             });
+  //       }
+  //     },
+  //   );
+  // } else {
+  //   return FutureBuilder<List<HotPlaceReplyResponse>>(
+  //     future: db.getHotPlaceReplysByHcId(cId),
+  //     builder: (context, snapshot) {
+  //       print(snapshot.data);
+  //       if (snapshot.connectionState == ConnectionState.waiting) {
+  //         // 데이터 로딩
+  //         return const Center(
+  //           child: CircularProgressIndicator(),
+  //         );
+  //       } else if (snapshot.hasError) {
+  //         // 오류
+  //         return const Center(
+  //           child: Text("댓글을 불러오는 중 오류가 발생했습니다."),
+  //         );
+  //       } else if (snapshot.hasData && snapshot.data!.isEmpty) {
+  //         // 댓글이 없는 경우
+  //         return Container();
+  //       } else {
+  //         // 댓글 목록
+  //         final replyList = snapshot.data!;
+  //         return ListView.builder(
+  //             shrinkWrap: true,
+  //             physics: const NeverScrollableScrollPhysics(),
+  //             itemCount: replyList.length,
+  //             itemBuilder: (BuildContext context, int index) {
+  //               return Column(
+  //                 children: [
+  //                   Container(
+  //                     height: 50.0,
+  //                     width: double.infinity,
+  //                     margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+  //                     child: Align(
+  //                         alignment: Alignment.centerLeft,
+  //                         child: Row(children: [
+  //                           const SizedBox(width: 20.0),
+  //                           SizedBox(
+  //                             width: 15.0,
+  //                             height: 15.0,
+  //                             child: Image.asset("assets/icons/reply_icon.png"),
+  //                           ),
+  //                           const SizedBox(width: 10.0),
+  //                           Column(
+  //                             crossAxisAlignment: CrossAxisAlignment.start,
+  //                             mainAxisAlignment: MainAxisAlignment.center,
+  //                             children: [
+  //                               Text(
+  //                                 replyList[index].nickname,
+  //                                 style: TextStyle(
+  //                                   color: AppTheme.mainTextColor,
+  //                                   fontSize: 12.0,
+  //                                   fontWeight: FontWeight.w600,
+  //                                 ),
+  //                               ),
+  //                               Row(
+  //                                   crossAxisAlignment: CrossAxisAlignment.end,
+  //                                   children: [
+  //                                     Text(
+  //                                       replyList[index].content,
+  //                                       style: TextStyle(
+  //                                           fontSize: 12.0,
+  //                                           color: AppTheme.mainTextColor,
+  //                                           fontWeight: FontWeight.w500),
+  //                                     ),
+  //                                     const SizedBox(width: 5.0),
+  //                                     // Align(alignment: Alignment., child:
+  //                                     Text(
+  //                                       replyList[index].date,
+  //                                       style: TextStyle(
+  //                                           fontSize: 6.0,
+  //                                           color: AppTheme.mainTextColor),
+  //                                       // )
+  //                                     )
+  //                                   ])
+  //                             ],
+  //                           ),
+  //                         ])),
+  //                   ),
+  //                 ],
+  //               );
+  //             });
+  //       }
+  //     },
+  //   );
+  // }
 }
