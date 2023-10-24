f17570377f218b8e91b90d8d3772e7abf23f7e85
mi-fasol <minyoong39@naver.com>
2023-10-24 18:34:11 +0900

    feat: hpId로 유저 받아오는 API 추가

--- lib/service/db_service.dart
+++ lib/service/db_service.dart
@@ -203,6 +203,19 @@ class DBService {
     }
   }
 
+  Future<UserResponse> getUserByHotPlace(int pId) async {
+    final response =
+        await http.get(Uri.parse("http://localhost:1004/hot/postUser/$pId"));
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body) as Map<String, dynamic>;
+      return UserResponse.fromJson(jsonData);
+    } else if (response.statusCode == 404) {
+      throw Exception("User not found");
+    } else {
+      throw Exception("Failed to fetch User by Post");
+    }
+  }
+
   Future<List<CommentResponse>> getCommentsByPId(int pId) async {
     final response = await http
         .get(Uri.parse('http://localhost:1004/postComment/commentPost/$pId'));
